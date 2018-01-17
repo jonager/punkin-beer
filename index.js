c936ffbde80d0d7e649833e4aaabd9e931740fb4
jonager <jonagerdev@gmail.com>
2018-01-16 22:25:59 -0500

    homepage finished, search bar added

--- index.js
+++ index.js
@@ -3,19 +3,28 @@
 // will hold all the favorites beers in the "db"
 let favorites = {};
 
-let wine = "http://lcboapi.com/products?page=4&per_page=100&q=wine";
-let beer = "http://lcboapi.com/products?per_page=100&q=budweiser";
+let wine = "http://lcboapi.com/products?page=&per_page=12&q=wine";
+let beer = "http://lcboapi.com/products?per_page=12&q=beer";
 
 
-function getBeer(url){
-    fetch(beer, {headers:{Authorization: 'Token MDpkNDU2MDIyYS1mYWRkLTExZTctYTFiNC1kM2RmZmI1YzBjNTM6dmU4SmQ2VE96aWtZN09yRnZrNW84VE5yNlZjQVQ2YTRIQlhw'}}) // Call the fetch function passing the url of the API as a parameter
+function getBeers(url){
+    fetch(url, {headers:{Authorization: 'Token MDpkNDU2MDIyYS1mYWRkLTExZTctYTFiNC1kM2RmZmI1YzBjNTM6dmU4SmQ2VE96aWtZN09yRnZrNW84VE5yNlZjQVQ2YTRIQlhw'}}) // Call the fetch function passing the url of the API as a parameter
         .then((resp) => resp.json())
         .then(function(data) {
             // Your code for handling the data you get from the API
-            console.log(data.result);
-            let img = document.querySelector('.beer-img');
-            img.setAttribute('src', data.result[0].image_url);
-            document.querySelector('.star').setAttribute('id', data.result[0].id);
+            let container = document.querySelector('.beers');
+            let result = data.result;
+            for (let i = 0; i < result.length; i++) {
+                container.innerHTML +=
+                `<div class="card">
+                    <img class="beer-img" src="${data.result[i].image_url}" alt="Avatar">
+                    <div class="info">
+                        <i class="material-icons star" id="${result[i].id}">star_border</i>
+                        <h4>${result[i].name}</h4> 
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint obcaecati unde quam magnam reprehenderit quod et deleniti provident sapiente sit,quod et deleniti provident sapiente sit.    ?</p> 
+                    </div>
+                </div>`;   
+            }
         })
         .catch(function() {
             // This is where you run code if the server returns any errors
@@ -35,8 +44,16 @@ function toggleFav(){
     }
 }
 
+function addClickEvent(collection){
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].addEventListener('click', toggleFav);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     //add favorite functionality
-    document.querySelector('.star').addEventListener('click', toggleFav);
-    getBeer();
+    getBeers(beer);
+    let stars = document.getElementsByClassName('star');
+    setTimeout(addClickEvent, 1000, stars);
 });
+
